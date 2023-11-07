7ff20baa0978b0d12942b8d725d0547eb7b479a3
Mahesh T <mahesh@numero.app>
2023-11-08 02:29:06 +0530

    Fix : Env dev.ts file changes

--- environments/environment.dev.ts
+++ environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   server: {
-    baseURL: 'https://eww6i2reaf.execute-api.us-east-1.amazonaws.com//api',
+    baseURL: 'https://eww6i2reaf.execute-api.us-east-1.amazonaws.com//api' ,
   }
 }
